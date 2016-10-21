3c5f79757c31b7e6190caf6758fe88274bd30002
xiaogu <gushoubang@126.com>
2016-10-21 11:36:02 +0800

    添加了rxjava，retrofit

--- app/src/main/java/com/xiaogu/ui/MainActivity.java
+++ app/src/main/java/com/xiaogu/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.xiaogu;
+package com.xiaogu.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.xiaogu.R;
 
 public class MainActivity extends AppCompatActivity {
 
